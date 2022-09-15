84586fe3627ce17a6579ad5acaa35c9709f179e6
Robert <109295306+rarellano5280@users.noreply.github.com>
2022-09-14 23:45:36 -0600

    added color coded time block function in JS

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-//added date and time to header using Moment.js
-var today = moment();
+// GIVEN I am using a daily planner to create a schedule
+//WHEN I open the planner
+//THEN the current day is displayed at the top of the calendar
+
+
+var today = moment(); //added date and time to header using Moment.js
 $("#currentDay").text(today.format('MMMM Do YYYY, h:mm a'));
+
+//WHEN I view the timeblocks for that day
+//THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+
+function colorOfTimeBlock() { //need to create a function that will allow me to determine what color needs to added to the time-block
+var hour = moment().hours(); //need to define a var for the hours within the time-block
+
+$('.time-block').each(function () {
+    console.log(typeof "time-block");
+    let presentHour = parseInt($(this).attr('id')); //Needed to convert time-block string into an integer so that I can compare equality
+
+    if (presentHour > hour) { 
+        $(this).addClass('future');
+    } else if (presentHour === hour) {
+        $(this).addClass('present');
+    } else {
+        $(this).addClass('past');
+        };
+    });
+};
+
+colorOfTimeBlock();
+
