ab492fed27fa1051da9ffda07fbf667cc1b643e5
Robert <109295306+rarellano5280@users.noreply.github.com>
2022-09-17 11:43:04 -0600

    fixed local storage issue, addd getItem function allow text to persist on reload

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
-//WHEN I click into a timeblock
-//THEN I can enter an event
-//WHEN I click the save button for that timeblock
-//THEN the text for that event is saved in local storage
-$(document).ready(function (){
-
-    $(".saveBtn").on("click" , function(){  
-
-        var event = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-
-        localStorage.setItem(time, event)
-    });
-
 // GIVEN I am using a daily planner to create a schedule
 //WHEN I open the planner
 //THEN the current day is displayed at the top of the calendar
-
-
 var today = moment();
 $("#currentDay").text(today.format('MMMM Do YYYY, h:mm a')); //Used moment.js to display current day and current time.
 
 //WHEN I view the timeblocks for that day
-//THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
+//THEN each timeblock is color coded to indicate whether it is in the past, present, or future  
 
 function colorOfTimeBlock() { //need to create a function that will allow me to determine what color needs to added to the time-block
 var hour = moment().hours(); //need to define a var for the hours within the time-block
 
 $('.time-block').each(function () {
-console.log(typeof "time-block");
+console.log(typeof "time-block"); //string
 let presentHour = parseInt($(this).attr('id'));  //Used parseInt to convert string into an interger so I can compare equality. 
 
 if (presentHour > hour){   //conditonal to assign correct color for time-blocks 
@@ -41,6 +24,33 @@ if (presentHour > hour){   //conditonal to assign correct color for time-blocks
 });
 };
 
+ //WHEN I click into a timeblock
+//THEN I can enter an event
+//WHEN I click the save button for that timeblock
+//THEN the text for that event is saved in local storage
+var saveBtn = $(".saveBtn"); 
 
-colorOfTimeBlock();
+saveBtn.on("click" , function() { //function to allow me to store the text that is within the textarea to local storage. 
+    var timeInput = $(this).siblings(".hour").text()
+    var savedInput = $(this).siblings(".description").val();
+    localStorage.setItem(timeInput, savedInput);
 });
+
+//WHEN I refresh the page
+//THEN the saved events persist
+function persistOnReload() { 
+    $(".hour").each(function() {
+        var presentHour = $(this).text();
+        var curDescrip = localStorage.getItem(presentHour);
+
+        if(curDescrip !== null) {
+            $(this).siblings(".description").val(curDescrip);
+        }
+    });
+}
+
+//Call functions to load. 
+colorOfTimeBlock();
+persistOnReload();
+
+
